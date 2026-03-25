0e101ee14fb6aed8d236bf230bf4568de9611b59
Jaan-mohammad <jd628559@gmail.com>
2026-03-25 23:31:58 +0530

    fix server router

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const path = require('path')
-const adminRoutes = require('./routes/admin.routes')
+
 
 
 dotenv.config()
@@ -14,35 +14,36 @@ const db = require('./config/db')
 // Import routes
 const authRoutes = require('./routes/auth.routes')
 const notesRoutes = require('./routes/notes.routes')
+const adminRoutes = require('./routes/admin.routes')
 
 const app = express()
 
-// Mount routes
+// Middlware 
 app.use(express.json())
 app.use(cors())
+
+// Server frontend
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/api/auth', authRoutes)
-app.use('/api/notes', notesRoutes)
-// Mount routes for Admin
+
+// Routers
 app.use('/api/auth', authRoutes)
 app.use('/api/notes', notesRoutes)
 app.use('/api/admin', adminRoutes)
 
-// Test db connection
-db.query('SELECT 1')
-  .then(() => console.log('Database connected successfully'))
-  .catch((err) => console.log('Database connection failed:', err))
-
-// Mount routes
-app.use('/api/auth', authRoutes)
 
 // Test route
 app.get('/api/test', (req, res) => {
   res.json({ message: 'paperDrop server is running' })
 })
 
+// Test DB connection
+db.query('SELECT 1')
+  .then(() => console.log('Database connected successfully'))
+  .catch((err) => console.log('Database connection failed:', err));
+
+// Start server
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
+});
 
