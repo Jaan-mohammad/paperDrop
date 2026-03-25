bf893b6156be2653d17c62af4ebd59d84e6e540c
Jaan-mohammad <jd628559@gmail.com>
2026-03-26 01:06:41 +0530

    update db config for production

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-// 1. Import mysql2
 const mysql = require('mysql2')
-
-// 2. Import dotenv to read .env file
 require('dotenv').config()
 
-// 3. Create a connection pool
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT, 
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+let pool
 
-// 4. Export the pool with promise support
+if (process.env.MYSQL_URL) {
+  // Production — Render MySQL URL
+  pool = mysql.createPool(process.env.MYSQL_URL)
+} else {
+  // Local — individual credentials
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+  })
+}
+
 module.exports = pool.promise()
-
-
